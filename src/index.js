2ec5952426cbb58ce5a822256532c684ff7d14ab
Hamza <hamza.husein017@gmail.com>
2021-01-18 20:50:29 +0530

    Create footer

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { FaTwitterSquare, FaReact } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 
+const hexCodeGenerator = () => {
+  const hexSymbols = "0123456789abcdef";
+  let color = "";
 
-const hexCodeGenerator = () =>{
-  const hexSymbols = '0123456789abcdef';
-  let color = '';
-
-  for(let i=0; i<6; i++){
-    const index = Math.floor(Math.random() * 16)
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * 16);
     color += hexSymbols[index];
   }
 
-  return ('#' + color);
-}
+  return "#" + color;
+};
 
 const generatedHexCode = hexCodeGenerator();
 
 const colorStyle = {
   backgroundColor: generatedHexCode,
-  position: 'absolute',
-  height: '100%',
-  width: '100%'
-}
+  position: "absolute",
+  height: "100%",
+  width: "100%",
+};
 
 // const titleStyle = {
 //   display: 'flex',
@@ -30,33 +31,32 @@ const colorStyle = {
 // }
 
 const hexCodeStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '100%',
-  
-}
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100%",
+};
 
 const refreshButtonStyle = {
-  marginTop: '1rem',
+  marginTop: "1rem",
   borderRadius: "1.5rem",
   fontSize: "1.5rem",
   background: "#000000",
-  color: '#ffffff',
+  color: "#ffffff",
   padding: "15px 20px 15px 20px",
   textDecoration: "none",
-  outline: 'none'
+  outline: "none",
 };
 
 const reload = () => {
   window.location.reload();
-}
+};
 
 const hex2rgba = (hex, alpha = 1) => {
   const [r, g, b] = hex.match(/\w\w/g).map((x) => parseInt(x, 16));
   return `rgba(${r},${g},${b},${alpha})`;
-}
+};
 
 const footerStyle = {
   backgroundColor: "white",
@@ -67,14 +67,14 @@ const footerStyle = {
 };
 
 const footerTextStyle = {
-  display: 'flex', 
-  justifyContent: 'center',
-  fontSize: '1.2rem',
-  alignItems: 'center',
-  height: '100%',
-}
+  display: "flex",
+  justifyContent: "center",
+  fontSize: "1.2rem",
+  alignItems: "center",
+  height: "100%",
+};
 
-const RandomColorGenerator = () =>{
+const RandomColorGenerator = () => {
   return (
     <div className="mainPage" style={colorStyle}>
       {/* <div className='title' style={titleStyle}><h1><u>Random Color Generator</u></h1></div> */}
@@ -97,17 +97,30 @@ const RandomColorGenerator = () =>{
       </div>
       <div className="footer" style={footerStyle}>
         <p style={footerTextStyle}>
-          © Copyright 2021 &nbsp;
+          Made with &nbsp;
+          <IconContext.Provider value={{ size: "1.7rem", color: "00d8ff" }}>
+            <div>
+              <FaReact />
+            </div>
+          </IconContext.Provider>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;© Copyright 2021 &nbsp;
           <a href="http://https://github.com/Hamdrive"> Hamza Husein</a>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <IconContext.Provider value={{ size: "1.6rem", color: "#00acee" }}>
+            <div>
+              <a href="http://https://twitter.com/LocalDiskHam">
+                <FaTwitterSquare />
+              </a>
+            </div>
+          </IconContext.Provider>
         </p>
       </div>
     </div>
   );
 };
 
-const App = () => <RandomColorGenerator />
+const App = () => <RandomColorGenerator />;
 
+const rootElement = document.getElementById("root");
 
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(<App />, rootElement)
+ReactDOM.render(<App />, rootElement);
